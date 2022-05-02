14d7de9d35035193e3120ae05de98ad106cd2c6e
yash-explorer <76256893+yash-explorer@users.noreply.github.com>
2022-05-02 15:27:30 +0530

    i added for loop.py ha ha ha

---
+++ forloop.py
@@ -0,0 +1,31 @@
+#111
+#222
+#333
+'''for i in range(1,4):
+    for j in range(1,4):
+        print(i,end="")
+    print()
+
+for i in range(1,4):
+    for j in range(1,i+1):
+        print(i,end="")
+    print()
+
+'''
+'''for i in range(1,3):
+    for j in range(1,i+1):
+        print(i,end="")
+    print()'''
+'''for i in range(1,4):
+    for j in range(1,4):
+        print(i,end="")
+    print()'''
+'''for i in range(r+1,1,-1):
+    for j in range(i,1,-1):
+        print(i,end="")
+    print()
+'''
+for i in range(0,4):
+    print("@"*i)
+for i in range(2,0,-1):
+    print("@"*i)
